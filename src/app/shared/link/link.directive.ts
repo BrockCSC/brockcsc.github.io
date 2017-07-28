52367079509011eb54a00fe524ee974a14c80120
fzxt <fahadahmad41@hotmail.com>
2017-07-27 23:23:45 -0400

    Add link component and directive

---
+++ src/app/shared/link/link.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[cscLink]'
+})
+export class LinkDirective {
+
+  constructor() { }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { ButtonDirective } from './button/button.directive';
+import { LinkComponent } from './src/app/shared/link/link.component';
+import { LinkDirective } from './link/link.directive';
 
 @NgModule({
     imports: [
         CommonModule
     ],
     exports: [ButtonDirective, ButtonComponent],
-    declarations: [ButtonComponent, ButtonDirective]
+    declarations: [ButtonComponent, ButtonDirective, LinkComponent, LinkDirective]
 })
 export class SharedModule { }
