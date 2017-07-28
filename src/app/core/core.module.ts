0960779926259548813ff62d6b39b1908deea2a8
Igor Laskaev <il13eo@brocku.ca>
2017-07-27 22:39:15 -0400

    Finished navbar styling. Added anchor tag as selector for button directive

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        SharedModule
     ],
     exports: [
         NavComponent,
--- src/app/shared/button/button.directive.ts
+++ src/app/shared/button/button.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
-    selector: 'button[csc-button]'
+    selector: 'button[csc-button], a[csc-button]'
 })
 export class ButtonDirective {
     @HostBinding('class.csc-button') btnClass = true;
