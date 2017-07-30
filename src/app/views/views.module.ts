c44f58d729fd773c2c4daf1ae04cb053427855f9
Igor Laskaev <il13eo@brocku.ca>
2017-07-30 19:08:48 -0400

    Added join us section. Updated styling on button directive

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        SharedModule
     ],
     exports: [HomeComponent],
     declarations: [HomeComponent]
