4df06d7f2820a72c950e6edaff82bf1ad4175583
Igor Laskaev <il13eo@brocku.ca>
2017-07-30 18:03:53 -0400

    Implemented events section

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+    events: Event[] = [];
+
     constructor() { }
 
     ngOnInit() {
+        this.initEvents();
     }
 
+    initEvents(): void {
+        this.events = [
+            {
+                icon: 'videogame_asset',
+                title: 'Gaming',
+                desc: 'Something here about games we play'
+            },
+            {
+                icon: 'code',
+                title: 'Techtalks & Seminars',
+                desc: 'We give a variety of talks throughout the year on all things software development'
+            },
+            {
+                icon: 'people_outline',
+                title: 'Social',
+                desc: 'Get to know fellow members and the department at our social events'
+            },
+            {
+                icon: 'add',
+                title: 'More',
+                desc: '... and much more!'
+            }
+
+        ];
+    }
+
+}
+
+interface Event {
+    icon: string;
+    title: string;
+    desc: string;
 }
