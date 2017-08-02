bd6ce3266cc73b49bf2bc67c8d02a4322ee7005a
Igor Laskaev <il13eo@brocku.ca>
2017-08-01 22:53:15 -0400

    Fixed styling errors with event creation form

--- src/app/shared/input-container/input-container.component.ts
+++ src/app/shared/input-container/input-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input , ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'csc-input-container',
@@ -10,8 +10,18 @@ export class InputContainerComponent implements OnInit {
     @Input() label: string;
     @Input() colsLabel = 3;
     @Input() colsInput = 9;
+    @Input() labelLocation = 'left'; // only left/top supported
 
     constructor() { }
 
     ngOnInit() { }
+
+    public getLabelCols(): number {
+        // We want to set column width to 100% so that label will be placed on top.
+        return this.labelLocation === 'left' ? this.colsLabel : 12;
+    }
+
+    public getInputCols(): number {
+        return this.labelLocation === 'left' ? this.colsInput : 12;
+    }
 }
