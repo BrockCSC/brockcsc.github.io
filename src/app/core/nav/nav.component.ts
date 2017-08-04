f8474f7c2b5c4177fd8332e9c72069b7e2cc29c8
fzxt <fahadahmad41@hotmail.com>
2017-08-04 14:14:04 -0400

    Refactor link directive, Change blue nav to white
    - Nav will now change the logo and link colours to a white variant on
    certain links
    
    - You can now specify the line colour on the link directive
    
    - Initialized the links inside of the component instead of hardcoding
    them in the template

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
@@ -9,9 +9,36 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
+    @ViewChild('logo') logo: ElementRef;
+
+    links: NavLink[];
+    colour = 'white';
+
     constructor(private _location: Location, private _router: Router) { }
 
     ngOnInit() {
+        this.initLinks();
+    }
+
+    initLinks(): void {
+        this.links = [
+            {
+                href: '',
+                desc: 'Team',
+            },
+            {
+                href: '',
+                desc: 'Events',
+            },
+            {
+                href: '',
+                desc: 'Services',
+            },
+            {
+                href: '',
+                desc: 'Contact',
+            }
+        ];
     }
 
     /*
@@ -19,17 +46,41 @@ export class NavComponent implements OnInit {
     we want to give the navbar some colour. This is done with the [ngClass] directive
     which adds the navy-background class to the root div of this view.
     */
-    public changeBackground(): boolean {
+    public changeToWhiteNav(): boolean {
         // TODO: Improve this whole logic or how the layout is done
         const currentPath = this._location.path();
         const noBgPaths = ['admin'];
 
         for (let i = 0; i < noBgPaths.length; i++) {
             if (currentPath.indexOf(noBgPaths[i]) !== -1) {
+                this.blackLogo();
+                this.setLinkColours('black');
                 return true;
             }
         }
+
+        this.whiteLogo();
+        this.setLinkColours('white');
         return false;
     }
 
+    private blackLogo() {
+        this.logo.nativeElement.src = 'assets/logo-black.svg';
+    }
+
+    private whiteLogo() {
+        this.logo.nativeElement.src = 'assets/logo.svg';
+    }
+
+    private setLinkColours(colour: string) {
+        this.links.forEach(link => {
+            link.colour = colour;
+        });
+    }
+}
+
+interface NavLink {
+    colour?: string;
+    href: string;
+    desc: string;
 }
--- src/app/shared/link/link.component.ts
+++ src/app/shared/link/link.component.ts
@@ -1,14 +1,37 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+        Component,
+        ViewEncapsulation,
+        ViewChild,
+        ElementRef,
+        Input,
+        AfterViewChecked,
+        HostListener
+    } from "@angular/core";
 
 @Component({
-    selector: "li[csc-link], a[csc-link]",
+    selector: "li[csclink], a[csclink]",
     templateUrl: "./link.component.html",
     styleUrls: ["./link.component.scss"],
     encapsulation: ViewEncapsulation.None
 })
-export class LinkComponent implements OnInit {
+export class LinkComponent implements AfterViewChecked {
+
+    @Input() csclink: string;
+    @ViewChild('line') line: ElementRef;
 
     constructor() {}
 
-    ngOnInit() {}
+    ngAfterViewChecked() {
+        this.line.nativeElement.style.background = this.csclink;
+    }
+
+    @HostListener('mouseover', [])
+    onHover() {
+        this.line.nativeElement.style.width = '100%';
+    }
+
+    @HostListener('mouseout', [])
+    onMouseOut() {
+        this.line.nativeElement.style.width = '0%';
+    }
 }
