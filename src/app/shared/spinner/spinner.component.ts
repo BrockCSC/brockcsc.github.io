73590ae6449a78e8c412c1bd5353831782a0aab7
Igor Laskaev <il13eo@brocku.ca>
2017-08-10 18:36:18 -0400

    Created a spinner component

---
+++ src/app/shared/spinner/spinner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'csc-spinner',
+  templateUrl: './spinner.component.html',
+  styleUrls: ['./spinner.component.scss']
+})
+export class SpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
