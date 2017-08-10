eb9e316518541615b0636db829e61edd71a0212d
Igor Laskaev <il13eo@brocku.ca>
2017-08-10 18:12:37 -0400

    Added various filtering methods that will be required for the events page

--- src/app/shared/api/event/event.service.ts
+++ src/app/shared/api/event/event.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Event } from './event';
 import { StorageService } from '../storage/storage.service';
+import { Thenable, Promise } from 'firebase';
+import { Query } from 'angularfire2/interfaces';
 
 @Injectable()
 export class EventApiService {
@@ -13,7 +15,46 @@ export class EventApiService {
         this.events = _db.list(this._path);
     }
 
-    public addEvent(event: Event): firebase.Thenable<Event> {
+    public getEventsWeek(): FirebaseListObservable<Event[]> {
+        const currDate = new Date();
+        const currTimestamp = currDate.valueOf();
+        const weekAheadTimestamp = currDate.setDate(currDate.getDate() + 7).valueOf();
+
+        return this.getEventsBetween(currTimestamp, weekAheadTimestamp);
+    }
+
+    public getEventsMonth(): FirebaseListObservable<Event[]> {
+        const currDate = new Date();
+        const weekAheadTimestamp = currDate.setDate(currDate.getDate() + 7).valueOf();
+        const monthAheadTimestamp = currDate.setMonth(currDate.getMonth() + 1).valueOf();
+
+        return this.getEventsBetween(weekAheadTimestamp, monthAheadTimestamp);
+    }
+
+    public getNextEvent(): FirebaseListObservable<Event[]> {
+        const currTimestamp = new Date().valueOf();
+        return this.queryEvent({
+            orderByChild: 'datetime/timeStartTimestamp',
+            startAt: currTimestamp,
+            limitToFirst: 1
+        });
+    }
+
+    private getEventsBetween(start: number, end: number): FirebaseListObservable<Event[]> {
+        return this.queryEvent({
+            orderByChild: 'datetime/timeStartTimestamp',
+            startAt: start,
+            endAt: end
+        });
+    }
+
+    private queryEvent(query: Query): FirebaseListObservable<Event[]> {
+        return this._db.list(this._path, {
+            query: query
+        });
+    }
+
+    public addEvent(event: Event): Thenable<Event> {
         return this.events.push(event);
     }
 
@@ -21,7 +62,7 @@ export class EventApiService {
         return this.events;
     }
 
-    public updateEvent(key: string, value: Event): firebase.Promise<void> {
+    public updateEvent(key: string, value: Event): Promise<void> {
         return this.events.update(key, value);
     }
 
@@ -29,7 +70,7 @@ export class EventApiService {
         return Promise.all(events.map(event => this.removeEvent(event)));
     }
 
-    public removeEvent(event: Event): firebase.Promise<void> {
+    public removeEvent(event: Event): Promise<void> {
         const image = event.image;
         const resources = event.resources;
 
@@ -44,7 +85,7 @@ export class EventApiService {
         return this.removeEventByKey(event.$key);
     }
 
-    public removeEventByKey(key: string): firebase.Promise<void> {
+    public removeEventByKey(key: string): Promise<void> {
         return this.events.remove(key);
     }
 
