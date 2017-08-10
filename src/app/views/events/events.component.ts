e7c705ccb82fdc1f5a4108615ba517819a5abe0a
Igor Laskaev <il13eo@brocku.ca>
2017-08-10 18:39:34 -0400

    Implemented basic layout and data retrieval of the events component

--- src/app/views/events/events.component.ts
+++ src/app/views/events/events.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { EventApiService, Event } from 'app/shared/api';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
-  selector: 'csc-events',
-  templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+    selector: 'csc-events',
+    templateUrl: './events.component.html',
+    styleUrls: ['./events.component.scss']
 })
 export class EventsComponent implements OnInit {
+    public eventsWeek: Event[] = [];
+    public eventsWeekLoaded = false;
+    public eventsMonth: Event[] = [];
+    public eventsMonthLoaded = false;
 
-  constructor() { }
+    constructor(private _eventApi: EventApiService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this._eventApi.getEventsWeek()
+            .subscribe(events => {
+                this.eventsWeekLoaded = true;
+                this.eventsWeek = events;
+            });
+
+        this._eventApi.getEventsMonth()
+            .subscribe(events => {
+                this.eventsMonthLoaded = true;
+                this.eventsMonth = events;
+            });
+    }
 
 }
