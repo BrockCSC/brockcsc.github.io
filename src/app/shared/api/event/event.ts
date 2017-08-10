f03f8ea41fc7bdb55d29b55fb88a549e9e9cdc18
Igor Laskaev <il13eo@brocku.ca>
2017-08-09 20:59:10 -0400

    Updated grid to be spaced out properly when using multiple columns in a row. Added a new event end property to Event. Updated admin event forms to reflect this change.

--- src/app/shared/api/event/event.ts
+++ src/app/shared/api/event/event.ts
@@ -4,11 +4,15 @@ export class Event {
     title: string;
     presenter: string;
     description: string;
-    date: string;
-    time: string;
     location: string;
     resources: CscFile[];
     image: CscFile;
-    timestamp: number;
     signupUrl: string;
+    datetime: {
+        date: string;
+        timeStart: string;
+        timeEnd: string;
+        timeStartTimestamp: number;
+        timeEndTimestamp: number;
+    };
 }
