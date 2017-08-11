c5964430f116977a114f28b4df6a1a05fd518037
Igor Laskaev <il13eo@brocku.ca>
2017-08-11 14:24:11 -0400

    Refactored events component, added unsubscribe to onDestroy

--- src/app/views/events/events.component.ts
+++ src/app/views/events/events.component.ts
@@ -1,32 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EventApiService, Event } from 'app/shared/api';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'csc-events',
     templateUrl: './events.component.html',
     styleUrls: ['./events.component.scss']
 })
-export class EventsComponent implements OnInit {
-    public eventsWeek: Event[] = [];
-    public eventsWeekLoaded = false;
-    public eventsMonth: Event[] = [];
-    public eventsMonthLoaded = false;
+export class EventsComponent implements OnInit, OnDestroy {
+    public eventsWeek: EventContainer;
+    public eventsMonth: EventContainer;
 
-    constructor(private _eventApi: EventApiService) { }
+    constructor(private _eventApi: EventApiService) {
+        this.eventsWeek = new EventContainer();
+        this.eventsMonth = new EventContainer();
+    }
 
-    ngOnInit() {
-        this._eventApi.getEventsWeek()
+    public ngOnInit(): void {
+        this.eventsWeek.subscription = this._eventApi.getEventsWeek()
             .subscribe(events => {
-                this.eventsWeekLoaded = true;
-                this.eventsWeek = events;
+                this.eventsWeek.loaded = true;
+                this.eventsWeek.events = events;
             });
 
-        this._eventApi.getEventsMonth()
+        this.eventsMonth.subscription = this._eventApi.getEventsMonth()
             .subscribe(events => {
-                this.eventsMonthLoaded = true;
-                this.eventsMonth = events;
+                this.eventsMonth.loaded = true;
+                this.eventsMonth.events = events;
             });
     }
 
+    public ngOnDestroy(): void {
+        this.eventsWeek.subscription.unsubscribe();
+        this.eventsMonth.subscription.unsubscribe();
+    }
+}
+
+class EventContainer {
+    events: Event[];
+    loaded: boolean;
+    subscription: Subscription;
+
+    constructor() {
+        this.events = [];
+        this.loaded = false;
+        this.subscription = null;
+    }
 }
