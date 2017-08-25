d7fdb5f293c61e2e7bbe5a627d6397acb3c55726
ilaskaev <i.laskaev@gmail.com>
2017-08-25 19:10:15 -0400

    Implemented contact form

--- src/app/views/contact/contact.component.ts
+++ src/app/views/contact/contact.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'csc-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+    selector: 'csc-contact',
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+    public googleForm: GoogleFormConfig;
+    @ViewChild('contactForm') contactForm;
 
-  constructor() { }
+    constructor() {
+        this.googleForm = {
+            url: 'https://docs.google.com/forms/d/e/1FAIpQLSd5odQ24nlsNQGLByLAzJwLgS7zFGb3GaEcAKRKqYGktpOrrQ/formResponse',
+            // post parameters for the google form
+            ids: {
+                name: 'entry.1340504895',
+                email: 'entry.1741416472',
+                message: 'entry.1141734855'
+            }
+        };
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
 
+    }
+
+    public onSubmit(): void {
+        this.contactForm.nativeElement.submit();
+        this.contactForm.nativeElement.reset();
+    }
+}
+
+class GoogleFormConfig {
+    url: string;
+    ids: {
+        name: string;
+        email: string;
+        message: string;
+    };
 }
