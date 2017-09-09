975873d3484b19013ecb4eb5f2f6b7e853808a83
Igor Laskaev <il13eo@brocku.ca>
2017-09-09 00:16:10 -0400

    Added intersection observer polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,6 +61,8 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * APPLICATION IMPORTS
  */
 
+ import 'intersection-observer';
+
 /**
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
