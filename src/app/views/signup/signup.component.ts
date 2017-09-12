a20374f932f5d034204ff68c33f2d8419aa4dde4
fzxt <fahadahmad41@hotmail.com>
2017-09-12 14:39:26 -0400

    signup: show completion message and send slack invite

--- src/app/views/signup/signup.component.ts
+++ src/app/views/signup/signup.component.ts
@@ -8,10 +8,17 @@ import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
 })
 export class SignupComponent {
     public form: FormGroup;
-    public googleForm: GoogleFormConfig;
+
+    public signupFormConfig: SignUpGoogleFormConfig;
+    public slackInviteFormConfig: SlackSignUpGoogleFormConfig;
+    public submitted: boolean;
+
     @ViewChild('signupForm') signupForm;
+    @ViewChild('slackInviteForm') slackInviteForm;
 
     constructor(private _formBuilder: FormBuilder) {
+        this.submitted = false;
+
         this.form = this._formBuilder.group({
             name: ['', [Validators.required]],
             student_id: ['', [Validators.required]],
@@ -22,7 +29,7 @@ export class SignupComponent {
             interests: ''
         });
 
-        this.googleForm = {
+        this.signupFormConfig = {
             url: 'https://docs.google.com/forms/d/e/1FAIpQLSetf2LuAor5VzbWRkm8T7wJ2MI7kvEzWKmwYCJt-sjniduyRQ/formResponse',
             ids: {
                 name: 'entry.789935118',
@@ -34,19 +41,26 @@ export class SignupComponent {
                 interests: 'entry.692315741'
             }
         };
+
+        this.slackInviteFormConfig = {
+            url: 'https://docs.google.com/forms/d/e/1FAIpQLSeqfI9wcilbJb-zb554SmQBZjWBwbTCVfzIo5OneNTC9BL4SA/formResponse',
+            ids: {
+                email: 'entry.347926706'
+            }
+        };
     }
 
     onSubmit(form: FormGroup) {
         if (form.valid) {
-            // TODO: Send invite to slack
             this.signupForm.nativeElement.submit();
-            this.signupForm.nativeElement.reset();
+            this.slackInviteForm.nativeElement.submit();
+            this.submitted = true;
         }
     }
 
 }
 
-class GoogleFormConfig {
+class SignUpGoogleFormConfig {
     url: string;
     ids: {
         name: string;
@@ -56,5 +70,12 @@ class GoogleFormConfig {
         email: string;
         number_of_years_member: string;
         interests: string;
+    };
+}
+
+class SlackSignUpGoogleFormConfig {
+    url: string;
+    ids: {
+        email: string;
     };
 }
