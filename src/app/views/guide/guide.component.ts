43e941b1f2e93d007e2dff4aeb4bd0f5c4aa0fbf
Dylan <dsouvage@gmail.com>
2020-07-11 13:21:12 -0400

    add guide markdown rendering

---
+++ src/app/views/guide/guide.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MarkdownService } from 'ngx-markdown';
+
+@Component({
+  selector: 'csc-guide',
+  templateUrl: './guide.component.html',
+  styleUrls: ['./guide.component.css']
+})
+export class GuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
