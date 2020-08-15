ce7fcd2d66aec6de9027afc8d2b21387b9301eef
Kindeep Singh Kargil <kindeepsingh@gmail.com>
2020-08-15 13:03:18 -0400

    Event cards

---
+++ src/app/views/events/event-card/event-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CscEvent } from './../../../shared/api/event/event';
+@Component({
+  selector: 'csc-event-card',
+  templateUrl: './event-card.component.html',
+  styleUrls: ['./event-card.component.scss'],
+})
+export class EventCardComponent implements OnInit {
+  @Input() event: CscEvent;
+  get date(): Date {
+    return new Date(this.event.datetime.date);
+  }
+  get start(): Date {
+    return new Date(this.event.datetime.timeStartTimestamp);
+  }
+  get end(): Date {
+    return new Date(this.event.datetime.timeEndTimestamp);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getImage(): string {
+    const url = this.event?.image?.url;
+    return `url(${url ? url : 'assets/placeholder.png'})`;
+  }
+}
