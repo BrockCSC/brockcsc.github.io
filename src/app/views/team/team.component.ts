de682cb938899fbfa086144882bc8fecf37bb7b3
CaptKillSwitch <62815232+CaptKillSwitch@users.noreply.github.com>
2021-07-11 21:37:40 -0400

    Team component CSS update

--- src/app/views/team/team.component.ts
+++ src/app/views/team/team.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { AngularFireList } from '@angular/fire/database';
 import { Exec, ExecApiService } from 'app/shared/api';
 import { Observable } from 'rxjs';
@@ -7,15 +14,45 @@ import { Observable } from 'rxjs';
   selector: 'csc-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss'],
+  animations: [
+    trigger('showPrevExecsAnim', [
+      state(
+        'show',
+        style({
+          overflow: 'hidden',
+          opacity: 1,
+          height: '*',
+        })
+      ),
+      state(
+        'hide',
+        style({
+          overflow: 'hidden',
+          opacity: 0,
+          height: '0px',
+        })
+      ),
+      transition('show => hide', animate('600ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in')),
+    ]),
+  ],
 })
 export class TeamComponent implements OnInit {
   execs: Observable<Exec[]>;
   prevExecs: Observable<Exec[]>;
-
+  showPrevExecs = false;
   constructor(private _api: ExecApiService) {}
 
   ngOnInit() {
     this.execs = this._api.getCurrentExecs();
     this.prevExecs = this._api.getPreviousExecs();
+  }
+
+  get prevDivState() {
+    return this.showPrevExecs ? 'show' : 'hide';
+  }
+
+  flipShowPrevExecs() {
+    this.showPrevExecs = !this.showPrevExecs;
   }
 }
